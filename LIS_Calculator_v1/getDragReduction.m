3d251f7de89e6e5744da3dded1328ccaacd19748
Matthew Fu <mkfu@princeton.edu>
2017-02-06 12:06:31 -0500

    rty

--- LIS_Calculator_v1/getDragReduction.m
+++ LIS_Calculator_v1/getDragReduction.m
@@ -3,6 +3,9 @@ k = 0.41;
 %k = 0.0394; 
 F0 = 3.2;
 %F0 = -54.669; 
+Finf = -0.8;
+a = 7;
+b = 0.7;
 
 %Get Re_tau
 Retau = ret;
@@ -13,7 +16,13 @@ lzplus = span;
 Rd = 0.5;
 Rl = -1;
 Rh = 1;
-temp2 = (1-Rd).*lxplus+sqrt(1-Rd)./k.*log(sqrt(1-Rd)*Retau)+sqrt(1-Rd).*(-0.8+(F0+0.8).*exp(-(lzplus.*sqrt(1-Rd)/7).^0.7));
+
+
+f0 = 3;
+finf = -1;
+
+%temp2 = (1-Rd).*lxplus+sqrt(1-Rd)./k.*log(sqrt(1-Rd)*Retau)+sqrt(1-Rd).*(Finf+(F0+Finf).*exp(-(lzplus.*sqrt(1-Rd)/a).^b));
+temp2 = (1-Rd).*lxplus+sqrt(1-Rd)./k.*log(sqrt(1-Rd)*Retau)+sqrt(1-Rd).*(finf+(f0-finf).^2./(f0-finf+lzplus));
 for n =1:100
     if temp>temp2
         Rh = Rd;
@@ -24,7 +33,7 @@ for n =1:100
     else
         break;
     end
-temp2 = (1-Rd).*lxplus+sqrt(1-Rd)./k.*log(sqrt(1-Rd)*Retau)+sqrt(1-Rd).*(-0.8+(F0+0.8).*exp(-(lzplus.*sqrt(1-Rd)/7).^0.7));
+temp2 = (1-Rd).*lxplus+sqrt(1-Rd)./k.*log(sqrt(1-Rd)*Retau)+sqrt(1-Rd).*(Finf+(F0+Finf).*exp(-(lzplus.*sqrt(1-Rd)/a).^b));
 end
 Rd = Rd*100;
 end
